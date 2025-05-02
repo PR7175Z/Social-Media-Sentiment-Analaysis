27aa14e5b3f7b7274a28f0d04868955f95666c63
PR7175Z <aadimpraz@gmail.com>
2025-05-02 06:37:44 +0545

    Testing api

---
+++ apicaller.py
@@ -0,0 +1,9 @@
+import requests
+
+# url = 'http://127.0.0.1:4444/predict'
+url = 'http://127.0.0.1:2000/predict'
+
+data = {'text': "This is not nice. I hate it."}
+response = requests.post(url, json=data)
+
+print(response.json())
