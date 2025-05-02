7ab085a1705a989cdd58a965ff5e6041e1ced98d
PR7175Z <aadimpraz@gmail.com>
2025-05-02 23:04:07 +0545

    API calling

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    async function get_response(inputtext) {
+        const features = {'text': inputtext}
+
+        const api = 'http://127.0.0.1:2000/predict';
+        const response = await fetch(api,{
+                method : 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify(features)
+            }
+        )
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    }
+
+    document.getElementById('inputForm').addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const inputValue = document.querySelector('textarea[name="caption"]').value;
+
+        async function displayResponse() {
+            try {
+                let response = await get_response( inputValue);
+
+                document.querySelector('.result').innerHTML = response;
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        }
+
+        displayResponse();
+    })
+})
